26d1f0deb48d985149b0a6b462af76d3aa2437f7
LANA.BI <LANA.BI@OOCL.COM>
2025-09-08 20:30:58 +0800

    feat: implement multiple cars fetch

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -14,7 +14,6 @@ public class ParkingLotTest {
         assertNotNull(parkingLot.park(car));
     }
 
-
     @Test
     public void Should_return_parked_car_when_fetch_given_a_parkingLot_with_a_parked_car_and_a_parking_ticket(){
         String carPlate="AAA1234";
@@ -23,5 +22,20 @@ public class ParkingLotTest {
         Ticket ticket=parkingLot.park(car);
         Car parkedCar=parkingLot.fetch(ticket);
         assertEquals(car,parkedCar);
+    }
+
+    @Test
+    public void Should_return_parked_cars_when_fetch_given_a_parkingLot_with_a_parked_car_and_parking_tickets(){
+        String carPlateA="AAA1234";
+        String carPlateB="BBB1234";
+        ParkingLot parkingLot=new ParkingLot();
+        Car carA=new Car(carPlateA);
+        Car carB=new Car(carPlateB);
+        Ticket ticketA=parkingLot.park(carA);
+        Ticket ticketB=parkingLot.park(carB);
+        Car parkedCarA=parkingLot.fetch(ticketA);
+        Car parkedCarB=parkingLot.fetch(ticketB);
+        assertEquals(carA,parkedCarA);
+        assertEquals(carB,parkedCarB);
     }
 }
