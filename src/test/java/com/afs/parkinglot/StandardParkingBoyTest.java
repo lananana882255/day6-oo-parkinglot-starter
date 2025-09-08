a048ffdba2a4ef680f4ec473064978adc681c1bb
LANA.BI <LANA.BI@OOCL.COM>
2025-09-08 21:29:18 +0800

    feat: implement multiple cars fetch function in StandardParkingBoy

--- src/test/java/com/afs/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/afs/parkinglot/StandardParkingBoyTest.java
@@ -26,4 +26,20 @@ public class StandardParkingBoyTest {
         Car parkedCar=parkingBoy.fetch(ticket);
         assertEquals(car,parkedCar);
     }
+
+    @Test
+    public void Should_return_parked_cars_when_fetch_car_given_a_parkingLot_with_a_parked_car_parking_boy_and_parking_tickets(){
+        String carPlateA="AAA1234";
+        String carPlateB="BBB1234";
+        ParkingLot parkingLot=new ParkingLot();
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(parkingLot);
+        Car carA=new Car(carPlateA);
+        Car carB=new Car(carPlateB);
+        Ticket ticketA=parkingBoy.park(carA);
+        Ticket ticketB=parkingBoy.park(carB);
+        Car parkedCarA=parkingBoy.fetch(ticketA);
+        Car parkedCarB=parkingBoy.fetch(ticketB);
+        assertEquals(carA,parkedCarA);
+        assertEquals(carB,parkedCarB);
+    }
 }
